85dd3438c4a9b1fcf4a3ed05b3245b649acbcb1d
Tom CAUFRIER <t.caufrier@student.helmo.be>
2023-08-31 09:46:30 +0200

    Add exo 1327

---
+++ 1327/exo.sql
@@ -0,0 +1,5 @@
+SELECT product_name,unit FROM Products JOIN (SELECT product_id,sum(unit) AS 'unit' FROM
+Orders
+WHERE month(order_date) = 2 AND year(order_date) = 2020
+GROUP BY product_id
+HAVING sum(unit) >=100)T ON Products.product_id = T.product_id

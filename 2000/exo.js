d860409e81d93f242bc41b6560b093bcff894d65
Tom C. <t.caufrier@icloud.com>
2024-05-02 21:43:54 +0200

    [EXO] 2000

---
+++ 2000/exo.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} word
+ * @param {character} ch
+ * @return {string}
+ */
+var reversePrefix = function(word, ch) {
+  const index = word.indexOf(ch);
+  if (index !== -1) {
+      return word.substring(0, index + 1).split('').reverse().join('') + word.substring(index + 1);
+  } else {
+      return word;
+  }
+};

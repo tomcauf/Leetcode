fe24ff41d1e0dace702c235b636267b7a8d41fd1
Tom C. <t.caufrier@icloud.com>
2024-05-02 21:16:32 +0200

    [EXO] 1431

---
+++ 1431/exo.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+  const maxCandies = Math.max(...candies); 
+  const result = [];
+
+  for (let i = 0; i < candies.length; i++) {
+      
+      result.push(candies[i] + extraCandies >= maxCandies);
+  }
+
+  return result;
+};

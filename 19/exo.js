71aa015d46d538f93f87fc9dafabaa0e7e923a27
Tom C. <t.caufrier@icloud.com>
2025-04-21 14:55:27 +0200

    [EXO] 19 - MEDIUM

---
+++ 19/exo.js
@@ -0,0 +1,25 @@
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function (head, n) {
+    const dummy = new ListNode(0);
+    dummy.next = head;
+
+    let fast = dummy;
+    let slow = dummy;
+
+    for (let i = 0; i <= n; i++) {
+        fast = fast.next;
+    }
+
+    while (fast !== null) {
+        fast = fast.next;
+        slow = slow.next;
+    }
+
+    slow.next = slow.next.next;
+
+    return dummy.next;
+};

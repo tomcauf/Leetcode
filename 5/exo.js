a2ee0f4cbe471b31cd6a6b9dd0d660bdb37f4a2c
Tom C. <t.caufrier@icloud.com>
2025-04-21 14:41:27 +0200

    [EXO] 5 - MEDIUM

---
+++ 5/exo.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function (s) {
+    if (s.length <= 1) return s;
+
+    let start = 0;
+    let maxLength = 1;
+    const expandAroundCenter = (left, right) => {
+        while (left >= 0 && right < s.length && s[left] === s[right]) {
+            const currentLength = right - left + 1;
+            if (currentLength > maxLength) {
+                maxLength = currentLength;
+                start = left;
+            }
+            left--;
+            right++;
+        }
+    };
+    for (let i = 0; i < s.length; i++) {
+        expandAroundCenter(i, i);
+    }
+    for (let i = 0; i < s.length - 1; i++) {
+        expandAroundCenter(i, i + 1);
+    }
+
+    return s.substring(start, start + maxLength);
+};
